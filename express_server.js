ee245dd1814f6173d03a38636f4ccdfca911b4fb
yiminmin <emily141@163.com>
2023-05-29 18:10:20 +0000

    added route for /urls

--- express_server.js
+++ express_server.js
@@ -10,6 +10,11 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 app.get("/set", (req, res) => {
  const a = 1;
  res.send(`a = ${a}`);
