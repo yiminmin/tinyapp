030dcd19721612ab44c2c5be260d8a6d6ebbab6c
yiminmin <emily141@163.com>
2023-05-29 19:47:41 +0000

    add urls_new route

--- express_server.js
+++ express_server.js
@@ -10,6 +10,12 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
