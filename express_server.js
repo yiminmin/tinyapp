2d7ee8addc20e104be3dbacbae9b4b0e71978365
yiminmin <emily141@163.com>
2023-05-29 18:38:34 +0000

    Added route for /urls/:id and created urls_show.ejs template

--- express_server.js
+++ express_server.js
@@ -15,6 +15,12 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+
+
 app.get("/set", (req, res) => {
  const a = 1;
  res.send(`a = ${a}`);
